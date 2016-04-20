3e83f912a74bb5f48065cc4ba700d7b84db1695f
Luis Borbolla <luis@4suredesign.com>
2016-04-20 15:47:17 -0500

    Ecoder 2 lados

--- encoder/encoder.py
+++ encoder/encoder.py
@@ -5,17 +5,40 @@ import math as m
 
 class Senal(object):
     """docstring for Senal"""
-    def __init__(self, pin):
-        self.pin = pin
-        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(self.pin, GPIO.RISING, callback=self.interrupcion, bouncetime=20)  
+    def __init__(self, pinA , pinB):
+        self.pinA = pinA
+        self.pinB = pinB
+        self.estado = 0
+        GPIO.setup(self.pinA, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.pinB, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+        GPIO.add_event_detect(self.pin, GPIO.RISING, callback=self.interrupcionA, bouncetime=1)  
+        GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=self.interrupcionB, bouncetime=1)
         self.cuenta = 0 
 
-    def interrupcion(self):
-    	self.cuenta += 1
+    def interrupcionA(self , pin):
+    	if self.estado:
+    	    self.cuenta += 1
+
+    	if not self.estado:
+    	    self.cuenta -= 1
+
     	print "La cuenta va en %s " % self.cuenta
 
+    def interrupcionB(self , pin):
+        if GPIO.input(self.pin):
+            self.estado = 1
+        if GPIO.input(self.pin) == 0:
+            self.estado = 0    	
+        print "Estado = %s" % self.estado
 
+
+
+        
+    
+
+		
 try:
     while True:
         time.sleep(1)

103cd6e556cea9553a31f6ff033ad9d7b79613b0
Luis Borbolla <luis@4suredesign.com>
2016-04-22 19:46:13 -0500

    1 intento de pigpio con 2 interrupciones para encoder

--- encoder/encoderpig.py
+++ encoder/encoderpig.py
@@ -1,16 +1,52 @@
 #!/usr/bin/env python
 
 import time
-
+import math as m 
 import pigpio # http://abyz.co.uk/rpi/pigpio/python.html
 
-pi = pigpio.pi()
-if not pi.connected:
-    exit(0)
 
-cb = pi.callback(21)
+class Signal(object):
+    """docstring for Interrupt"""
+    def __init__(self, pinA , pinB):
+        self.pinA      = pinA
+        self.pinB      = pinB
+        self.gpio      = pigpio.pi()
+        self.perimetro = 40*m.pi
+        self.cuenta    = 0
+        self.estado    = 0
+        self.tiempo_actual = 0 
+        self.tiempo_anterior = time.time()
+        self.velocidad = 0
 
-while True:
-    print(cb.tally())
-    cb.reset_tally()
-    time.sleep(1)
+        #Definicion de funciones que correran al interrumpirse en los pines
+        self.interrupcionA  = gpio.callback(self.pinA, edge = pigpio.FALLING_EDGE ,func = self.guia)
+        self.interrupcionB = gpio.callback(self.pinB, edge = pigpio.EITHER_EDGE , func = self.referencia)
+        
+
+    def guia(self):
+        self.tiempo_actual = time.time()
+        if self.estado:
+        	self.cuenta += 1
+
+        if not self.estado:
+            self.cuenta -= 1
+
+        tiempo = self.tiempo_actual-self.tiempo_anterior
+        self.tiempo_anterior  = self.tiempo_actual
+        self.velocidad = (self.perimetro/1000)/tiempo    
+
+    def referencia(self):
+        if int(gpio.read(self.pinB)):
+            self.estado = 1
+        
+        if not int(gpio.read(self.pinB)):
+            self.estado = 0                         
+		
+def main():
+    
+    encoder = Signal(21,20)
+    try:
+    	while 1:
+            time.sleep(1)		
+    except KeyboardInterrupt:
+        pi.clear_bank_1(bin(2**20+2**21))

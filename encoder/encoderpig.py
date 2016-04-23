b02153c23ad7e67e972944a211095788b74a5f52
Luis Borbolla <luis@4suredesign.com>
2016-04-22 20:06:39 -0500

    1 intento de pigpio con 2 interrupciones para encoder

--- encoder/encoderpig.py
+++ encoder/encoderpig.py
@@ -50,7 +50,7 @@ def main():
     	while 1:
             time.sleep(1)		
     except KeyboardInterrupt:
-        gpio.clear_bank_1(bin(2**20+2**21))
+        self.gpio.clear_bank_1(bin(2**20+2**21))
 
 if __name__ == '__main__':
     main()        

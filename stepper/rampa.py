44fca223e6c4a29a0edb3d3d2855071f1e214acf
desktop <luis@4suredesign.com>
2016-04-26 18:27:21 -0500

    Rampa stepper

--- stepper/rampa.py
+++ stepper/rampa.py
@@ -1,72 +1,45 @@
-#!/usr/bin/env python
-
 import time
-
 import pigpio
-
-START_DELAY=4000
-FINAL_DELAY=100
-STEP=100
-
+    
+WAVES=5
 GPIO=26
-
-pi = pigpio.pi()
-
-pi.set_mode(GPIO, pigpio.OUTPUT)
-
-pi.wave_clear()
-
-# short waveform to repeat final speed
-
-wf=[]
-
-wf.append(pigpio.pulse(1<<GPIO, 0,       FINAL_DELAY))
-wf.append(pigpio.pulse(0,       1<<GPIO, FINAL_DELAY))
-
-pi.wave_add_generic(wf)
-
-wid0 = pi.wave_create()
-
-# build initial ramp
-
-wf=[]
-
-for delay in range(START_DELAY, FINAL_DELAY, -STEP):
-   wf.append(pigpio.pulse(1<<GPIO, 0,       delay))
-   wf.append(pigpio.pulse(0,       1<<GPIO, delay))
-
-pi.wave_add_generic(wf)
-
-# add lots of pulses at final rate to give timing lee-way
-
-wf=[]
-
-# add after existing pulses
-
-offset = pi.wave_get_micros()
-
-print("ramp is {} micros".format(offset))
-
-wf.append(pigpio.pulse(0, 0, offset))
-
-for i in range(2000):
-   wf.append(pigpio.pulse(1<<GPIO, 0,       FINAL_DELAY))
-   wf.append(pigpio.pulse(0,       1<<GPIO, FINAL_DELAY))
-
-pi.wave_add_generic(wf)
-
-wid1 = pi.wave_create()
-
-# send ramp, stop when final rate reached
-
-pi.wave_send_once(wid1)
-
-time.sleep(float(offset)/1000000.0) # make sure it's a float
-
-pi.wave_send_repeat(wid0)
-
-time.sleep(1)
-
-pi.wave_tx_stop()
-
+       
+wid=[0]*WAVES
+       
+pi = pigpio.pi() # Connect to local Pi.
+       
+pi.set_mode(GPIO, pigpio.OUTPUT);
+ 
+for i in range(WAVES):
+   pi.wave_add_generic([
+      pigpio.pulse(1<<GPIO, 0, 20),
+      pigpio.pulse(0, 1<<GPIO, (i+1)*200)]);
+      
+   wid[i] = pi.wave_create();
+       
+pi.wave_chain([
+   wid[4], wid[3], wid[2],       # transmit waves 4+3+2
+   255, 0,                       # loop start
+      wid[0], wid[0], wid[0],    # transmit waves 0+0+0
+      255, 0,                    # loop start
+         wid[0], wid[1],         # transmit waves 0+1
+         255, 2, 0x88, 0x13,     # delay 5000us
+      255, 1, 30, 0,             # loop end (repeat 30 times)
+      255, 0,                    # loop start
+         wid[2], wid[3], wid[0], # transmit waves 2+3+0
+         wid[3], wid[1], wid[2], # transmit waves 3+1+2
+      255, 1, 10, 0,             # loop end (repeat 10 times)
+   255, 1, 5, 0,                 # loop end (repeat 5 times)
+   wid[4], wid[4], wid[4],       # transmit waves 4+4+4
+   255, 2, 0x20, 0x4E,           # delay 20000us
+   wid[0], wid[0], wid[0],       # transmit waves 0+0+0
+   ])
+      
+while pi.wave_tx_busy():
+   time.sleep(0.1);
+ 
+for i in range(WAVES):
+   pi.wave_delete(wid[i])
+ 
 pi.stop()
+ 

29e71df2a71341e8574098711ef426a639c061a1
desktop <luis@4suredesign.com>
2016-04-26 00:34:34 -0500

    Operaciones con nema42

--- stepper/operaciones.py
+++ stepper/operaciones.py
@@ -1,32 +1,56 @@
 import time
-def vel2pulsos( velocidad , distancia):
-    perimetro = 125.66370614359172 #mm
-    pulsos = velocidad * 2000 / perimetro
-    slp = 1./(2*pulsos)
-    pasos = (distancia/perimetro)*2000
-    print round(pasos)
-    print "Funcionando a una velocidad de %s mm/s \t a %s pulsos cada segundo."%(velocidad,pulsos)
-    cuenta = 0
-    t1 = time.clock()
-    try:
-        for i in xrange(0,int(round(pasos))):
-            #GPIO.output(self.pin_pulse,False)
-            cuenta += 1
-            time.sleep(slp)
-            #GPIO.output(self.pin_pulse,True)
-            time.sleep(slp)
-        t2 = time.time()    
-        print "Se dieron %s pasos , deberian de ser %s en %s segundos" % (cuenta , pasos , t2-t1)
+
+class Encoder(object):
+    """docstring for Encoder"""
+    def __init__(self, arg):
+        super(Encoder, self).__init__()
+        self.arg = arg
+
+    def vel2pulsos( velocidad , distancia):
+        perimetro = 125.66370614359172 #mm
+        pulsos = velocidad * 2000 / perimetro
+        slp = 1./(2*pulsos)
+        pasos = (distancia/perimetro)*2000
+        print round(pasos)
+        print "Funcionando a una velocidad de %s mm/s \t a %s pulsos cada segundo."%(velocidad,pulsos)
+        cuenta = 0
+        t1 = time.clock()
+        try:
+            for i in xrange(0,int(round(pasos))):
+                #GPIO.output(self.pin_pulse,False)
+                cuenta += 1
+                time.sleep(slp)
+                #GPIO.output(self.pin_pulse,True)
+                time.sleep(slp)
+            t2 = time.time()    
+            print "Se dieron %s pasos , deberian de ser %s en %s segundos" % (cuenta , pasos , t2-t1)
+            
+
+
+
+
+        except KeyboardInterrupt:
+            print "Terminado de girar"
+            #GPIO.cleanup()
+
+
+class Nema42(object):
+    """docstring for Nema42"""
+    def __init__(self):
+        self.perimetro = 125.66370614359172
+        self.dist_x_pulso = self.perimetro/2000
+
+    def ts_from_vel(self , velocidad):
+        num_pulsos = velocidad/self.dist_x_pulso
+        ts = 1./(2*num_pulsos)
+
+        return ts
+
+
         
-
-
-
-
-    except KeyboardInterrupt:
-        print "Terminado de girar"
-        #GPIO.cleanup()
-
-
+        
+def pulsos2vel():
+    pass
 def procedure():
     time.sleep(2.5)
 # measure process time

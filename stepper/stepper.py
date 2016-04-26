ec18cf1a8a4f516c6084834fb44388163a6533e3
desktop <luis@4suredesign.com>
2016-04-26 15:42:30 -0500

    velocidad en encoder mecanico

--- stepper/stepper.py
+++ stepper/stepper.py
@@ -90,7 +90,7 @@ class Nema42(object):
 if __name__ == '__main__':
     traccion = Nema42(19,26)
     for i in xrange(1000,1,-1):
-        traccion.ts_2_freq(i/100000.)
+        traccion.ts_2_freq(0.005)
 
 
 

f28972e2b6cfcf72aba1fe8d0d75683d3576780e
desktop <luis@4suredesign.com>
2016-04-28 22:53:12 -0500

    Pre3paracion imports para clase de PID velocidad :O

--- PID/operaciones.py
+++ PID/operaciones.py
@@ -52,21 +52,3 @@ class Nema42(object):
 
         
         
-def pulsos2vel():
-    pass
-def procedure():
-    time.sleep(2.5)
-# measure process time
-t0 = time.clock()
-procedure()
-print time.clock() - t0, "seconds process time"
-
-# measure wall time
-t0 = time.time()
-procedure()
-print time.time() - t0, "seconds wall time"        
-
-
-procedure()
-
-#vel2pulsos(10,10)

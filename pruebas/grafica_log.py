46b315026f9dd8c71844959a371c176032ea9acd
Luis Borbolla <luis@4suredesign.com>
2016-05-18 18:52:42 -0500

    Agregado de direccion a velocidad

--- pruebas/grafica_log.py
+++ pruebas/grafica_log.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = "datos_prbs3s.log"
+filename = "datos_prbs1s.log"
 
 with open(filename) as f:
     header = f.readline().split("\t")
@@ -17,7 +17,7 @@ ax1.plot(data['corrida'] , data['set_point'] ,'g')
 
 ax2.plot(data['corrida'] , data['velocidad'] , 'r')
 
-ax1.set_title("Prueba PRBS para PID de velocidad")
+ax1.set_title("Prueba PRBS para PID de velocidad 2s")
 ax1.set_xlabel('Corrida')
 ax1.set_ylabel('Set Point')
 

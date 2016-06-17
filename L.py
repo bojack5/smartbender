ca9fd0665d7ecdb18c0169a46ae58f4d5b29963b
desktop <luis@4suredesign.com>
2016-06-17 00:30:16 -0500

    comienzo de programa de prueba

---
+++ L.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from rotary_encoder2 import decoder
+from cilindros.cilindro import Sencillo , Doble
+
+tn = Doble(0,1)
+m1 = Sencillo(2)
+m2 = Sencillo(3)
+pr = Sencillo(4)
+ci = Sencillo(5)
+db = Doble(6,7)
+
+magnitudes = []
